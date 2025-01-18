336005626f8e0acbdf1effde825e52d0e28fd4e3
Ishaan Kumar <ishaan@descript.com>
2025-01-18 17:18:54 +0000

    add lip sync

--- video_dub/video.py
+++ video_dub/video.py
@@ -5,15 +5,35 @@ import subprocess
 import cv2
 import math
 import numpy as np
+from argparse import ArgumentParser
+
+LATENT_SYNC_PATH = Path("/workspace/LatentSync")
 
 
 def get_frame_number(time, fps):
-    # return math.ceil(time * fps)
-    # return round(time * fps)
     return int(time * fps)
 
 
 def read_n_next_frames(cap, n, get_scores=False):
+    """
+    Read n frames from a video capture object.
+
+    Parameters
+    ----------
+    cap : cv2.VideoCapture
+        Video capture object
+    n : int
+        Number of frames to read
+    get_scores : bool, optional
+        If True, return frame scores
+
+    Returns
+    -------
+    frames : List[np.ndarray]
+        List of frames
+    scores : List[float]
+        List of frame scores, if `get_scores` is `True`
+    """
     frames = []
     scores = [0]
     prev_frame = None
@@ -38,18 +58,30 @@ def read_n_next_frames(cap, n, get_scores=False):
 def interpolate_frames(frame1, frame2, factor):
     """
     Create intermediate frames between two frames using linear interpolation.
-
-    Args:
-        frame1: First frame
-        frame2: Second frame
-        factor: Float between 0 and 1 indicating position between frames
     """
     return cv2.addWeighted(frame1, 1 - factor, frame2, factor, 0)
 
 
-def gen_video_cv(
-    input_path, output_path, gen_audio_spans, orig_audio_spans, target_audio_duration
-):
+def gen_video_cv(input_path, output_path, gen_audio_spans, orig_audio_spans):
+    """
+    Generate a new video by editing the original video based on the audio spans.
+
+    If the generated audio is shorter than the original audio, the video is squeezed.
+    If the generated audio is longer than the original audio, the video is extended.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the input video file
+    output_path : str
+        Path to the output video file
+    gen_audio_spans : List[Tuple[float, float]]
+        List of audio spans for the generated audio
+    orig_audio_spans : List[Tuple[float, float]]
+        List of audio spans for the original audio
+    target_audio_duration : float
+        Target audio duration for the generated audio
+    """
 
     #  Open the video file
     cap = cv2.VideoCapture(input_path)
@@ -65,19 +97,28 @@ def gen_video_cv(
     prev_span_end = 0
     start_frame = 0
     end_frame = 0
+    # Iterate over edited spans
+    # When we encounter a new span, we read frames from the previous
+    # span end to the current span start. Then we read frames corresponding
+    # to the edited span.
+    # If edited span is shorter than original span, we reduce frames by
+    # dropping frames with less significant changes.
+    # If edited span is longer than original span, we extend frames by
+    # frame interpolation.
     for orig_span, gen_span in zip(orig_audio_spans, gen_audio_spans):
         orig_span_start, orig_span_end = orig_span
 
         orig_span_len = orig_span_end - orig_span_start
         gen_span_len = gen_span[1] - gen_span[0]
 
+        # Unedited section. We take as much as frames as we can.
         if orig_span_start > prev_span_end:
-            # Unedited section. We take as much as frames as we can.
             start_frame = end_frame
             end_frame = get_frame_number(orig_span_start, input_fps)
             final_frames.extend(read_n_next_frames(cap, end_frame - start_frame))
 
         prev_span_end = orig_span_end
+        # Delete scenario
         if gen_span_len <= 0.1:
             start_frame = end_frame
             end_frame = get_frame_number(orig_span_end, input_fps)
@@ -92,10 +133,9 @@ def gen_video_cv(
             frames, frame_scores = read_n_next_frames(
                 cap, end_frame - start_frame, get_scores=True
             )
-            print("red frames", len(frames))
-            print("frame scores", len(frame_scores))
             frame_scores = frame_scores / np.max(frame_scores)
             desired_frames = get_frame_number(gen_span_len, input_fps) + 1
+
             #  Select frames based on scores
             selected_frames = [frames[0]]  # Always include first frame
             total_score = np.sum(frame_scores)
@@ -109,9 +149,12 @@ def gen_video_cv(
                 if accumulated_frames >= 1:
                     selected_frames.append(frames[i])
                     accumulated_frames -= 1
+
             selected_frames.append(frames[-1])  # Always include last frame
             num_selected = len(selected_frames)
             deficit = desired_frames - num_selected
+
+            # If we have a deficit, we interpolate between the last two frames
             if deficit > 0:
                 last_frame = selected_frames.pop(-1)
                 deficit_frames = []
@@ -123,20 +166,18 @@ def gen_video_cv(
                     )
                 selected_frames.extend(deficit_frames)
                 selected_frames.append(last_frame)
-            print("desired squeezed frames", desired_frames)
-            print("framed selected", len(selected_frames))
+
             final_frames.extend(selected_frames)
         else:
             # Extend frames
             end_frame = get_frame_number(orig_span_end, input_fps)
             snippet_frames = read_n_next_frames(cap, end_frame - start_frame)
             num_original_frames = len(snippet_frames)
-            print("desired frames", end_frame - start_frame)
-            print("read frames", num_original_frames)
-            print("total frames", len(final_frames), "max frames", total_frames)
+
             desired_frames = get_frame_number(gen_span_len, input_fps)
             num_interp_frames = int(desired_frames / (num_original_frames - 1))
             extended_snippet_frames = []
+
             for i in range(num_original_frames - 1):
                 frame1 = snippet_frames[i]
                 frame2 = snippet_frames[i + 1]
@@ -150,20 +191,21 @@ def gen_video_cv(
                 for j in range(num_interp_frames):
                     # Calculate interpolation factor
                     factor = j / num_interp_frames
-
-                    # Create and write interpolated frame
                     interp_frame = interpolate_frames(frame1, frame2, factor)
                     extended_snippet_frames.append(interp_frame)
+
             extended_snippet_frames.append(snippet_frames[-1])
-            print("extended frames", len(extended_snippet_frames))
-            print("desired frame", desired_frames)
             final_frames.extend(extended_snippet_frames)
 
+    # Leftover video
     if prev_span_end < video_duration:
-        start_frame = end_frame  # get_frame_number(prev_span_end, input_fps)
+        start_frame = end_frame
         end_frame = total_frames
         final_frames.extend(read_n_next_frames(cap, end_frame - start_frame))
+
     cap.release()
+
+    # Write the final video
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     out = cv2.VideoWriter(output_path, fourcc, input_fps, (frame_width, frame_height))
     for frame in final_frames:
@@ -171,7 +213,70 @@ def gen_video_cv(
     out.release()
 
 
-def main(video_path, output_path, original_transcript, new_transcript):
+def gen_video_sync_lips(input_path, output_path, gen_audio_path, gen_audio_duration):
+    """
+    Use LatentSync to generate a new video with synced lips.
+
+    When the generated audio is longer than the original audio, the video is
+    extended by concatenating the original video with its reversed version.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the input video file
+    output_path : str
+        Path to the output video file
+    gen_audio_path : str
+        Path to the generated audio file
+    gen_audio_duration : float
+        Duration of the generated audio
+
+    """
+    # Get video duration
+    cap = cv2.VideoCapture(input_path)
+    input_fps = int(cap.get(cv2.CAP_PROP_FPS))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_duration = total_frames / input_fps
+    cap.release()
+
+    if video_duration < gen_audio_duration:
+        # Increase video duration.
+        # Create sequence of original and reversed video clips to minimize
+        # visual artifacts.
+
+        reverse_video_path = output_path.parent / "input_reverse.mp4"
+        temp_video_path = output_path.parent / "input_extended.mp4"
+        revert_command = f"-i {input_path} -vf reverse -an {reverse_video_path}"
+        mult_factor = math.ceil(gen_audio_duration / video_duration)
+        concat_file = output_path.parent / "concat.txt"
+        concat_content = [
+            f"file '{input_path if i % 2 == 0 else reverse_video_path}'"
+            for i in range(mult_factor)
+        ]
+        concat_file.write_text("\n".join(concat_content))
+        concat_command = f"-f concat -safe 0 -i {concat_file} -c copy {temp_video_path}"
+        commands = [revert_command, concat_command]
+        print("\n".join(commands))
+        for command in commands:
+            subprocess.check_output(
+                f"ffmpeg -y -hide_banner -loglevel error {command}", shell=True
+            )
+        input_path = temp_video_path
+
+    command = f"""
+        python -m scripts.inference \
+        --unet_config_path "configs/unet/second_stage.yaml" \
+        --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
+        --inference_steps 20 \
+        --guidance_scale 1.5 \
+        --video_path "{input_path}" \
+        --audio_path "{gen_audio_path}" \
+        --video_out_path "{output_path}"
+    """
+    subprocess.check_call(command, shell=True, cwd=str(LATENT_SYNC_PATH))
+
+
+def main(video_path, output_path, original_transcript, new_transcript, sync_lips=False):
     with TemporaryDirectory() as tmp_dir:
         tmp_dir = Path(tmp_dir)
         input_audio_path = tmp_dir / "audio.wav"
@@ -194,69 +299,66 @@ def main(video_path, output_path, original_transcript, new_transcript):
             ]
         )
 
-        gen_audio_path = output_path.parent / "gen_audio.wav"
-
         # Generate new audio
+        gen_audio_path = output_path.parent / "gen_audio.wav"
         gen_audio_spans, orig_audio_spans, gen_audio_duration = gen_audio(
             input_audio_path, gen_audio_path, original_transcript, new_transcript
         )
 
-        print("gen audio spans", gen_audio_spans)
-        print("orig audio spans", orig_audio_spans)
-
         # Generate new video
-        gen_video_path = output_path.parent / "gen_video.mp4"
-        gen_video_cv(
-            video_path,
-            gen_video_path,
-            gen_audio_spans,
-            orig_audio_spans,
-            gen_audio_duration,
-        )
-
-        # Combine new audio with video
-        subprocess.check_output(
-            [
-                "ffmpeg",
-                "-hide_banner",
-                "-loglevel",
-                "error",
-                "-y",
-                "-i",
+        if sync_lips:
+            gen_video_sync_lips(
+                video_path, output_path, gen_audio_path, gen_audio_duration
+            )
+        else:
+            gen_video_path = output_path.parent / "gen_video.mp4"
+            gen_video_cv(
+                video_path,
                 gen_video_path,
-                "-i",
-                gen_audio_path,
-                "-c:v",
-                "copy",
-                "-c:a",
-                "aac",
-                "-strict",
-                "experimental",
-                output_path,
-            ]
-        )
+                gen_audio_spans,
+                orig_audio_spans,
+            )
+
+            # Combine new audio with video
+            subprocess.check_output(
+                [
+                    "ffmpeg",
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-y",
+                    "-i",
+                    gen_video_path,
+                    "-i",
+                    gen_audio_path,
+                    "-c:v",
+                    "copy",
+                    "-c:a",
+                    "aac",
+                    "-strict",
+                    "experimental",
+                    output_path,
+                ]
+            )
 
 
 if __name__ == "__main__":
-    video_path = Path(__file__).parent.parent / "LatentSync/assets/demo3_video.mp4"
-    output_path = Path(__file__).parent / "out.mp4"
-    original_transcript = "For a long time. Also this was the first time in disneyland for both of us. We've never been to disneyland in any other city. So, first of all, because we live in another city, we need to travel to Shanghai on Gaojia. It's a speed train that connects different cities in China."
-    new_transcript = "For a long time. Also this was the first time in disneyland for both of us. We've never been to disneyland in any other city. So, first of all, because we live in another city, we need to register ourselves at the passport office. It's a speed train that connects different cities in China."
-    # new_transcript = "For a long time. Also this was going to be a new day in disneyland for both of us. We've never been to disneyland in any other city. So, first of all, because we live in another city, we need to travel to Shanghai on Gaojia. It's a super fast train that connects different cities in China."
-
-    video_path = Path(__file__).parent.parent / "LatentSync/assets/demo5_video.mp4"
-    original_transcript = "When I was a kid, I feel like you heard the thing, you heard the term, don't cry. you don't need to cry. crying is the most beautiful thing you can do. I encourage people to cry. I cry all the time. And I think it's the most healthy expression of how are you feeling and I, I sometimes wish."
-    new_transcript = "When I was a kid, I feel like you heard the thing, you heard the term, don't cry. It always kind of unsettling to me. crying is the most beautiful thing you can do. I encourage people to cry. It'a great habit after all. And I think it's the most healthy expression of how are you feeling and I, I sometimes wish."
-
-    video_path = (
-        Path(__file__).parent.parent / "LatentSync/assets/akana_shorter_norm.mp4"
+    argparser = ArgumentParser(help="Edit video based on new transcript")
+    argparser.add_argument("--input_path", type=Path, required=True)
+    argparser.add_argument("--output_path", type=Path, required=True)
+    argparser.add_argument("--original_transcript", type=str, required=True)
+    argparser.add_argument("--new_transcript", type=str, required=True)
+    argparser.add_argument(
+        "--sync_lips",
+        action="store_true",
+        help="When specified, the video will be lip-synced",
     )
-    original_transcript = "Pretty silly. Little fucking fool. Now a word of caution. Do not ask people this if you are not ready to hear the answer. I was and am really down for criticism of myself as a human being because I want to be better. I am just naturally curious what people think of me. And I had the time to mentally prepare to hear their opinions without any defense"
-    new_transcript = "Pretty silly. Little fucking fool. Let me give you some advice first. Do not ask people this if you are not ready to hear the answer. I was and am really down for criticism of myself as a human being because I want to be better. I am just naturally curious what people have to say about me. And I had the time to mentally prepare to hear their opinions without any defense"
+    args = argparser.parse_args()
 
-    video_path = Path(__file__).parent.parent / "LatentSync/assets/wolf_norm.mp4"
-    original_transcript = "Still with me? Okay, now comes the fun part. Let's build you a wolf avatar. Youll be able to choose the fur colour, tail length, floppy or pointy ears. I choose floppy. And the colour of your liking. Great. Almost ready to go. Now that your wolf page is set up, it's time to invite your friends. So now click save."
-    new_transcript = "Still with me? Okay, now comes the fun part. Let's build you a wolf avatar. Youll be able to choose the size of the head and shape of the nose. I choose floppy. And the colour of your liking. Great. Now you are going to wait a little bit. Now that your wolf page is set up, it's time to invite your friends. So now click save."
-    main(video_path, output_path, original_transcript, new_transcript)
-# ==================ddddd==i==sss==============================
-# edited durations [(5.53, 5.49), (5.97, 5.97), (6.43, 7.87)]
+    main(
+        args.input_path,
+        args.output_path,
+        args.original_transcript,
+        args.new_transcript,
+        args.sync_lips,
+    )
